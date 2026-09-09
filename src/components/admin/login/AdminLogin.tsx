38876f1d58531a0e4cec6747eceafb9b218bd8bc
Shieunhe <bryanlibante09@gmail.com>
2026-09-09 10:25:22 +0800

    login ui ready to implement

---
+++ src/components/admin/login/AdminLogin.tsx
@@ -0,0 +1,7 @@
+export function AdminLogin() {
+  return (
+    <div>
+      <h1>Admin Login LOGIC AND DESIGN HERE</h1>
+    </div>
+  );
+}
---
+++ src/components/user/login/UserLogin.tsx
@@ -0,0 +1,7 @@
+export function UserLogin() {
+  return (
+    <div>
+      <h1>This is login</h1>
+    </div>
+  );
+}
