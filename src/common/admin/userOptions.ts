c562a805531fcb13cd6ca76b4a6500bf6a875039
Shieunhe <bryanlibante09@gmail.com>
2026-09-11 13:25:04 +0800

    add logic backend

---
+++ src/common/admin/userOptions.ts
@@ -0,0 +1,11 @@
+export const PERMANENT_ADDRESS = "Bonbon, Clarin, Bohol";
+
+export const puroks = [
+  "Purok 1",
+  "Purok 2",
+  "Purok 3",
+  "Purok 4",
+  "Purok 5",
+  "Purok 6",
+  "Purok 7",
+] as const;
---
+++ src/types/user.ts
@@ -0,0 +1,24 @@
+export const USER_ROLE = 1;
+
+export type UserStatus = "pending" | "registered" | "not registered";
+
+export type Users = {
+  id: number;
+  role: typeof USER_ROLE;
+  first_name: string;
+  last_name: string;
+  middle_name: string;
+  date_of_birth: string;
+  suffix: string;
+  age: number;
+  civil_status: string;
+  sex: string;
+  permanent_address: string;
+  purok: string;
+  contact_number: string;
+  email: string;
+  status: UserStatus;
+  decline_reason: string;
+  create_date: string;
+  update_date: string;
+};
